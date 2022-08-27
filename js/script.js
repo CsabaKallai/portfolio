12a46579e99cd544cf96f5e5dbecd64d774325a2
CsabaKallai <71966031+CsabaKallai@users.noreply.github.com>
2022-08-27 14:37:51 +0200

    Animated scroll down button has been added and swiper js has been removed

--- js/script.js
+++ js/script.js
@@ -1,21 +1,19 @@
-new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+// new Swiper('.swiper', {
+//     direction: 'horizontal',
+//     speed: 500,
+//     autoplay: {
+//       delay: 5000
+//     },
+//     pagination: {
+//       el: '.swiper-pagination'
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev'
+//     }
+// });
+
+AOS.init({
+  delay: 500, // values from 0 to 3000, with step 50ms
+  duration: 400
+});
